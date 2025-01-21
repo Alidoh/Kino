7e503b5f79b04f20b9aae85dce8bf5debe19b387
Alidoh <144313493+Alidoh@users.noreply.github.com>
2025-01-21 17:19:03 +0100

    Update Form1.cs

--- Kino/Kino/Form1.cs
+++ Kino/Kino/Form1.cs
@@ -8,12 +8,11 @@ namespace Kino
         public Form1()
         {
             InitializeComponent();
-            groupBox2.Visible = false;
-            groupBox3.Visible = false;
-            groupBox5.Visible = false;
-            groupBox6.Visible = false;
-            groupBox7.Visible = false;
-            groupBox8.Visible = false;
+            Control[] groupBoxes = { groupBox2, groupBox3, groupBox5, groupBox6, groupBox7, groupBox8 };
+            for (int i = 0; i < groupBoxes.Length; i++)
+            {
+                groupBoxes[i].Visible = false;
+            }
         }
         private List<Button> seznamtlacitek = new List<Button>();
         private void Form1_Load(object sender, EventArgs e)
@@ -23,32 +22,29 @@ namespace Kino
 
         private void button1_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < groupBoxes.Length; i++)
+            {
+                groupBoxes[i].Visible = false;
+            }
             groupBox2.Visible = true;
-            groupBox3.Visible = false;
-            groupBox5.Visible = false;
-            groupBox6.Visible = true;
-            groupBox7.Visible = false;
-            groupBox8.Visible = false;
             Button clickedButton = sender as Button;
             seznamtlacitek.Add(clickedButton);
         }
         private void button2_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < groupBoxes.Length; i++)
+            {
+                groupBoxes[i].Visible = false;
+            }
             groupBox3.Visible = true;
-            groupBox2.Visible = false;
-            groupBox5.Visible = false;
-            groupBox7.Visible = true;
-            groupBox6.Visible = false;
-            groupBox8.Visible = false;
         }
         private void button3_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < groupBoxes.Length; i++)
+            {
+                groupBoxes[i].Visible = false;
+            }
             groupBox5.Visible = true;
-            groupBox3.Visible = false;
-            groupBox2.Visible = false;
-            groupBox8.Visible = true;
-            groupBox7.Visible = false;
-            groupBox6.Visible = false;
         }
         private void groupBox1_Enter(object sender, EventArgs e)
         {
